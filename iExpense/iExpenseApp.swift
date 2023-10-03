8c1f90babecee92ebcc28e47da7ea48e2bd87bda
ZachMommaerts <zmommaerts@gmail.com>
2023-10-02 20:50:28 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Zach Mommaerts on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
