29f5b61f542b739885ca4d693fddbeba50f1faf7
zmommaerts <zmommaerts@apple.com>
2023-10-03 13:11:06 -0500

    - Create AddView to add expenses to list - Create logic to show new sheet

---
+++ iExpense/AddView.swift
@@ -0,0 +1,39 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Zach Mommaerts on 10/3/23.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Expense", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
