507c4935903ddf466fe2ebe0d898f39d28a99b52
zmommaerts <zmommaerts@apple.com>
2023-10-03 13:37:31 -0500

    - Use UserDefaults to save Data

--- iExpense/Expenses.swift
+++ iExpense/Expenses.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+class Expenses: ObservableObject{
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
